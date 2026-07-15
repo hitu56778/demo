88b71ae6e1f8303d71b8509f95026e7f16c83adc
hitu56778 <hiteshgiri459@gmail.com>
2026-07-15 22:11:45 +0530

    Add files via upload

---
+++ hitesh.py
@@ -0,0 +1,38 @@
+import  streamlit as st
+
+
+st.title(" *welcome to coffee club *")
+
+
+
+st.image("kk.jpg")
+
+st.markdown("Coffee is more than a morning pick-me-up; it is a rich sensory experience backed by health benefits. Moderate consumption can boost metabolism, improve focus, and deliver a hearty dose of antioxidants and polyphenols, which support heart and brain health"
+            )
+st.header("The health & wellness angle")
+st.markdown(" Coffee is one of the largest sources of antioxidants in the modern diet, helping your body fight free radicals.Brain Power: The caffeine in coffee blocks the inhibitory neurotransmitter adenosine, which increases alertness, improves mood, and enhances cognitive function.Nutrient Dense: A single cup of coffee contains essential nutrients like vitamins B2, B5, B3, manganese, and potassium.Long-Term Protection: Regular consumption has been linked to a lower risk of neurodegenerative diseases, including Alzheimer's and Parkinson's."
+            "")
+st.image("images (1).jpg")
+st.header(" $All availlable "
+          "")
+
+
+
+st.header("Cappuccino")
+
+st.image("download.jpg")
+st.markdown("A classic cappuccino is an Italian coffee drink made with equal parts espresso, steamed milk, and thick milk foam. Typically served in a 6 oz cup, it delivers a bolder, more espresso-forward flavor than a latte."
+            "")
+st.header("Latte")
+st.image("latte.jpg")
+
+st.markdown("A latte (short for caffè latte) is a classic espresso-based coffee")
+
+st.header("THAKNS FOR VISITING :-")
+
+
+
+
+
+
+
